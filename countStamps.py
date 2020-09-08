ae3418159be0779d4e216ce894fd807b8ca6dd5c
MarcosValenzuela <marcosvalezuela@mail.fresnostate.edu>
2020-09-08 16:44:12 -0700

    finished lab

--- countStamps.py
+++ countStamps.py
@@ -1,10 +1,15 @@
 import math
 sheets = 16
 
-exactvalue = sheets/5
+def calculate(sheets):
+    answer = sheets/5
+    print('-------------------------')
+    print('Sheets:', sheets)
+    print('The answer is: ',answer)
+    stamps = math.ceil(answer)
+    print ('Rounded:',stamps,'!')
+    print('-------------------------')
+    return stamps
+output = calculate(16)
 
-print('-------------------------')
-print('Before rounding up: ',exactvalue)
-stamps = math.ceil(exactvalue)
-print ('You need this many stamps',stamps,'!')
-print('-------------------------')
+print("the return statement is: ", output)
