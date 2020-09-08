d40160016040e3901e780d6f8126e6914a3305a5
MarcosValenzuela <marcosvalezuela@mail.fresnostate.edu>
2020-09-08 16:20:30 -0700

    first commit

---
+++ countStamps.py
@@ -0,0 +1,10 @@
+import math
+sheets = 16
+
+exactvalue = sheets/5
+
+print('-------------------------')
+print('Before rounding up: ',exactvalue)
+stamps = math.ceil(exactvalue)
+print ('You need this many stamps',stamps,'!')
+print('-------------------------')
